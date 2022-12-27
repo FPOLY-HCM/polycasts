da363ee92dc37453ed031fc17d6d4dea732aa2a0
Ngo Quoc Dat <datlechin@gmail.com>
2022-12-27 21:12:20 +0700

    Format code

--- database/migrations/2022_12_20_173206_create_conversations_table.php
+++ database/migrations/2022_12_20_173206_create_conversations_table.php
@@ -1,13 +1,13 @@
 <?php
 
 use App\Models\Channel;
-use App\Models\Reply;
 use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
     /**
      * Run the migrations.
      */
