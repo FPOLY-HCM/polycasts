92f2ce9ed97a93d032e67a47472bcffcc41ab5c2
Ngo Quoc Dat <datlechin@gmail.com>
2023-01-08 01:24:44 +0700

    Add Filament

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,13 +5,17 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
+use Filament\Models\Contracts\HasName;
+use Filament\Models\Contracts\FilamentUser;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser, HasName
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens;
+    use HasFactory;
+    use Notifiable;
 
     protected $fillable = [
         'username',
@@ -28,6 +32,16 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function canAccessFilament(): bool
+    {
+        return str_ends_with($this->email, '@polycasts.com') && $this->hasVerifiedEmail();
+    }
+
+    public function getFilamentName(): string
+    {
+        return $this->username;
+    }
 
     public function profile(): HasOne
     {
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,12 +5,21 @@ namespace Database\Seeders;
 use App\Models\Profile;
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
     public function run(): void
     {
         User::truncate();
+
+        User::create([
+            'username' => 'admin',
+            'email' => 'admin@polycasts.com',
+            'password' => Hash::make('123456'),
+            'email_verified_at' => Carbon::now(),
+        ]);
 
         User::factory(100)
             ->has(Profile::factory(), 'profile')
